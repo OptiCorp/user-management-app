4f2ff9e4350854da2039f2082efd87a600648c91
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-10-31 09:51:23 +0100

    using msal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import { Route, Routes } from 'react-router-dom'
 import Users from './pages/users/Users'
 import AddUser from './pages/users/AddUser'
+import { useIsAuthenticated } from '@azure/msal-react'
+import { Login } from './pages/login'
 
 function App() {
+    const isAuthenticated = useIsAuthenticated()
     return (
         <>
-            <Routes>
-                <Route path="/" element={<Users />} />
-                <Route path="/AddUser" element={<AddUser />} />
-                <Route path="/EditUser/:id" element={<AddUser />} />
-            </Routes>
+            {isAuthenticated && (
+                <Routes>
+                    <Route path="/" element={<Users />} />
+                    <Route path="/AddUser" element={<AddUser />} />
+                    <Route path="/EditUser/:id" element={<AddUser />} />
+                </Routes>
+            )}
+            {!isAuthenticated && <Login />}
         </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import App from './App.tsx'
 import './index.css'
 import GlobalStyles from './style/GlobalStyles.ts'
 import { BrowserRouter } from 'react-router-dom'
+import { MsalProvider } from '@azure/msal-react'
+import { pca } from './msalConfig.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <BrowserRouter>
-            <GlobalStyles />
-            <App />
-        </BrowserRouter>
+        <MsalProvider instance={pca}>
+            <BrowserRouter>
+                <GlobalStyles />
+                <App />
+            </BrowserRouter>
+        </MsalProvider>
     </React.StrictMode>
 )
