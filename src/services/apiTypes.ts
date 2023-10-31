ae54ae73774ac356fe1b8a0a26b1f82150f908bd
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-10-31 10:03:22 +0100

    api service types

---
+++ src/services/apiTypes.ts
@@ -0,0 +1,17 @@
+export type User = {
+    id: string
+    azureAdUserId: string
+    firstName: string
+    lastName: string
+    email: string
+    username: string
+    userRole: UserRole
+    status: string
+    createdDate: string
+    updatedDate: string | null
+}
+
+export type UserRole = {
+    id: string
+    name: string
+}
