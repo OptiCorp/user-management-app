be5602fd9c79f5a50b4e9503ed0208fa111b2d57
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-10-31 09:17:06 +0100

    colors

--- src/style/GlobalStyles.ts
+++ src/style/GlobalStyles.ts
@@ -1,5 +1,22 @@
 import { createGlobalStyle } from 'styled-components'
 
+export const COLORS = {
+    white: '#FFF',
+    black: '#000',
+    gray: '#BEBEBE',
+    lightGray: '#CCC',
+    silverGray: '#DCDCDC',
+    paleGray: '#EAEAEA',
+    frostyGray: '#F0F3F3',
+    lightSteelBlue: '#DEEDEE',
+    primary: '#007079',
+    secondary: '#243746',
+    cautionaryYellow: '#FBCA36',
+    warningOrange: '#ED8936',
+    dangerRed: '#EB0000',
+    whiteSmoke: '#f5f5f5',
+}
+
 const GlobalStyles = createGlobalStyle`
     body {
         margin: 0 auto;
