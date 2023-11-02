b05f847760914082dc0c5896f3e55055c4a52f80
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-11-02 15:24:38 +0100

    header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,87 +1,53 @@
 import { Icon, TopBar } from '@equinor/eds-core-react'
 import { arrow_back_ios, menu } from '@equinor/eds-icons'
 import { useEffect, useState } from 'react'
-import { useLocation, useNavigate } from 'react-router'
+import { useLocation, useNavigate, useParams } from 'react-router'
 import { COLORS } from '../../style/GlobalStyles'
 import Sidebar from '../sidebar/Sidebar'
 import { HeaderLocation, NewTopBar } from './styles'
+import { User } from '../../services/apiTypes'
+import apiService from '../../services/api'
 
 export const Header = () => {
+    const api = apiService()
     const navigate = useNavigate()
-    const [activeUrl, setActiveUrl] = useState<string>('')
-    const location = useLocation()
+    const { id } = useParams() as { id: string }
+    const appLocation = useLocation()
     const [open, setOpen] = useState(false)
+    const [activeUrl, setActiveUrl] = useState<string>('')
+    const [title, setTitle] = useState<string | undefined>()
 
-    /* const [title, setTitle] = useState('') */
+    const path = appLocation.pathname.split('/')
+
+    const hasPath = (paths: string[]) => {
+        for (let i = 1; i < paths.length; i++) {
+            if (paths[i] === '') return false
+        }
+        return true
+    }
+
     useEffect(() => {
         setActiveUrl(window.location.pathname)
-    }, [location])
-
-    /* const useBasePath = () => {
-        const params = useParams<Record<string, string>>()
-
-        return Object.values(params).reduce(
-            (path, param) => path?.replace('/' + param, ''),
-            location.pathname.slice(1)
-        )
-    } */
-
-    /* const { id, workflowId, taskId, punchId } = useParams() as {
-        id: string
-        taskId: string
-        workflowId: string
-        punchId: string
-    } */
-
-    /* useEffect(() => {
-        let pathTitle = ''
-
-        if (location.pathname.includes('FillOutCheckList')) {
-            pathTitle =
-                workflow?.checklist.title + ' ' + workflow?.id.slice(10, -18) ||
-                ''
-        } else if (location.pathname === '/AddUser/') {
-            pathTitle = 'Add user'
-        } else if (location.pathname === '/ListUsers/') {
-            pathTitle = 'List of users'
-        } else if (location.pathname.includes('PreviewCheckList')) {
-            pathTitle = checklist?.title || ''
-        } else if (location.pathname.includes('ForReviewChecklists')) {
-            pathTitle = 'For review'
-        } else if (location.pathname.includes('EditCheckList')) {
-            pathTitle = 'Edit' + ' ' + checklist?.title || ''
-        } else if (location.pathname === '/SendCheckList/') {
-            pathTitle = 'Send checklist' || ''
-        } else if (location.pathname === `/SendChecklist/${id}`) {
-            pathTitle = 'Send' + ' ' + checklist?.title || ''
-        } else if (
-            location.pathname === `/workflow/${workflowId}/punch/${punchId}`
-        ) {
-            pathTitle =
-                (workflow?.checklist.title || '') +
-                ' ' +
-                ' Punch ' +
-                punchId.slice(0, -28)
-        } else if (location.pathname.includes(workflowId && taskId)) {
-            pathTitle = 'Create punch'
-        } else if (location.pathname.includes('Checklists')) {
-            pathTitle = `${
-                isLeader ? 'Checklists in progress' : 'Outgoing checklists'
-            }`
-        } else if (location.pathname.includes('MyCheckLists')) {
-            pathTitle = `${
-                isLeader ? 'Checklists templates' : 'Incomming checklists'
-            }`
-        } else {
-            pathTitle =
-                basePath?.match(/[A-Z][a-z]+|[0-9]+/g)?.join('') ||
-                basePath ||
-                ''
+        if (!hasPath(path)) {
+            setTitle('Users')
         }
-        setTitle(pathTitle)
-    }, [location.pathname, basePath, workflow, checklist]) */
+        if (hasPath(path)) {
+            const newPath = path.pop()
+            console.log(newPath === id)
+            if (newPath === id) {
+                api.getUser(id).then((userData: User) => {
+                    if (userData) {
+                        setTitle(`${userData.firstName} ${userData.lastName}`)
+                    }
+                })
+            } else {
+                const newTitle = newPath?.match(/[A-Z][a-z]+/g)?.join(' ')
+                setTitle(newTitle)
+            }
+        }
+    }, [location, path, id])
 
-    const onClick = () => {
+    const navigateBack = () => {
         navigate(-1)
     }
 
@@ -91,11 +57,11 @@ export const Header = () => {
             <NewTopBar>
                 <TopBar.Header>
                     {activeUrl === '/' ? null : (
-                        <Icon data={arrow_back_ios} color={COLORS.white} onClick={onClick} />
+                        <Icon data={arrow_back_ios} color={COLORS.white} onClick={navigateBack} />
                     )}
                 </TopBar.Header>
                 <TopBar.CustomContent>
-                    <HeaderLocation>title</HeaderLocation>
+                    <HeaderLocation>{title}</HeaderLocation>
                 </TopBar.CustomContent>
                 <TopBar.Actions>
                     <Icon
