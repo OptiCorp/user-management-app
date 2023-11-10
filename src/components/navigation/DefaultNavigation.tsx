4f968f1e834c45fff94741ad932395895193d5dd
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-11-10 15:31:04 +0100

    adding dynamic color to nav icon

--- src/components/navigation/DefaultNavigation.tsx
+++ src/components/navigation/DefaultNavigation.tsx
@@ -1,12 +1,25 @@
 import { group } from '@equinor/eds-icons'
 import { NavItem } from './NavItem'
 import { FooterContainer, StyledList, StyledTab } from './style'
+import { useLocation } from 'react-router'
+import { COLORS } from '../../style/GlobalStyles'
 
 export const DefaultNavigation: React.FC<{}> = () => {
+    const appLocation = useLocation()
+    const path = appLocation.pathname.replace(/^\/|\/$/g, '')
+
     return (
         <FooterContainer>
             <StyledList>
-                <StyledTab>{<NavItem icon={group} to="/" />}</StyledTab>
+                <StyledTab>
+                    {
+                        <NavItem
+                            icon={group}
+                            color={path === '' ? COLORS.gray : COLORS.white}
+                            to="/"
+                        />
+                    }
+                </StyledTab>
             </StyledList>
         </FooterContainer>
     )
--- src/components/navigation/NavItem.tsx
+++ src/components/navigation/NavItem.tsx
@@ -4,11 +4,21 @@ import { Link } from 'react-router-dom'
 import { ImageContainerActive } from './style'
 import { COLORS } from '../../style/GlobalStyles'
 
-export const NavItem = ({ name, icon, to }: { name?: string; icon: IconData; to: string }) => {
+export const NavItem = ({
+    name,
+    icon,
+    to,
+    color,
+}: {
+    name?: string
+    icon: IconData
+    to: string
+    color?: string
+}) => {
     return (
         <Link to={to} style={{ textDecoration: 'none' }}>
             <ImageContainerActive>
-                <Icon data={icon} size={24} color={COLORS.white} />
+                <Icon data={icon} size={24} color={color} />
                 <Typography variant="caption" color={COLORS.white}>
                     {name}
                 </Typography>
